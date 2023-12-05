301c9563b91bf737ce2154eccc5433a5730fb063
Iosif-Claudiu Ceaca <ceacaiosifclaudiu@gmail.com>
2023-12-05 17:48:32 +0200

    Improving code in HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,65 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScrollView, View } from 'react-native';
-import { fetchTopRatedMovies, fetchTrendingMovies, fetchUpcomingMovies } from "../api/MovieDB";
+import { fetchTopRatedMovies, fetchTrendingMovies, fetchUpcomingMovies } from '../api/MovieDB';
+import { backgroundColor } from '../commonStyle';
 import HeaderHomeScreen from '../components/HeaderHomeScreen';
 import Loading from '../components/Loading';
 import MovieList from '../components/MovieList';
 import TrendingMovies from '../components/TrendingMovies';
-import { backgroundColor } from '../commonStyle';
 import { Movie } from '../types/types';
 
 const HomeScreen = () => {
-    const [trending, setTrending] = React.useState<Movie[]>([]);
-    const [upcoming, setUpcoming] = React.useState<Movie[]>([]);
-    const [topRated, setTopRated] = React.useState<Movie[]>([]);
-    const [loading, setLoading] = React.useState(true);
+    const [trending, setTrending] = useState<Movie[]>([]);
+    const [upcoming, setUpcoming] = useState<Movie[]>([]);
+    const [topRated, setTopRated] = useState<Movie[]>([]);
+    const [loading, setLoading] = useState(true);
 
-    React.useEffect(() => {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const [trendingData, upcomingData, topRatedData] = await Promise.all([
+                    fetchTrendingMovies(),
+                    fetchUpcomingMovies(),
+                    fetchTopRatedMovies(),
+                ]);
+
+                setTrending(trendingData.results || []);
+                setUpcoming(upcomingData.results || []);
+                setTopRated(topRatedData.results || []);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
         fetchData();
     }, []);
-
-    const fetchData = async () => {
-        await Promise.all([getTrendingMovies(), getUpcomingMovies(), getTopRatedMovies()]);
-        setLoading(false);
-    };
-
-    const getTrendingMovies = async () => {
-        const data = await fetchTrendingMovies();
-        if (data && data.results) setTrending(data.results)
-        setLoading(false)
-    }
-    const getUpcomingMovies = async () => {
-        const data = await fetchUpcomingMovies();
-        if (data && data.results) setUpcoming(data.results)
-    }
-
-    const getTopRatedMovies = async () => {
-        const data = await fetchTopRatedMovies();
-        if (data && data.results) setTopRated(data.results)
-    }
 
     return (
         <View style={{ flex: 1, backgroundColor: backgroundColor }}>
             <HeaderHomeScreen />
 
-            {
-                loading ?
-                    <Loading />
-                    :
-                    <ScrollView showsVerticalScrollIndicator={false}>
-                        {
-                            trending.length > 0 &&
-                            <TrendingMovies trending={trending} />
-                        }
+            {loading ? (
+                <Loading />
+            ) : (
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    {trending.length > 0 && <TrendingMovies trending={trending} />}
 
-                        <MovieList title='Upcoming' data={upcoming} />
+                    <MovieList title="Upcoming" data={upcoming} />
 
-                        <MovieList title='Top Rated' data={topRated} />
-                    </ScrollView>
-            }
-
+                    <MovieList title="Top Rated" data={topRated} />
+                </ScrollView>
+            )}
         </View>
-    )
-}
+    );
+};
 
-export default HomeScreen
+export default HomeScreen;
